e1a9b9e56065d79eb177ef7814b5a096049d1e98
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2023-04-16 20:17:50 +0100

    Change default ScrollPane skin to Conscious Scroll Pane skin and add styles for new skin

--- transit-samples/src/main/java/com/pixelduke/samples/transit/ControlsSample.java
+++ transit-samples/src/main/java/com/pixelduke/samples/transit/ControlsSample.java
@@ -74,8 +74,8 @@ public class ControlsSample extends Application {
 
     private static final String COLOR_PICKER_RESOURCE = "Transit ColorPicker.fxml";
 
-    static final private String RESOURCE = TOGGLE_SWITCH_RESOURCE;
-    static final private Style STARTING_STYLE = Style.LIGHT;
+    static final private String RESOURCE = SCROLL_PANE_RESOURCE;
+    static final private Style STARTING_STYLE = Style.DARK;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
