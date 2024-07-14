9ec1bf1de0521e2fa1cda47bfd00f175357a1654
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2024-07-14 21:43:08 +0100

    Convert Theme to user-agent-stylesheet: Set Spinner arrow color back to -fx-mark-color

--- transit-samples/src/main/java/com/pixelduke/samples/transit/ControlsSample.java
+++ transit-samples/src/main/java/com/pixelduke/samples/transit/ControlsSample.java
@@ -74,7 +74,7 @@ public class ControlsSample extends Application {
 
     private static final String COLOR_PICKER_RESOURCE = "Transit ColorPicker.fxml";
 
-    static final private String RESOURCE = CHOICE_BOX_RESOURCE;
+    static final private String RESOURCE = SPINNER_RESOURCE;
     static final private Style STARTING_STYLE = Style.LIGHT;
 
     @Override
